f122d0693c9a3f87180fb48f15081d622f6b0965
Jamey Hicks <jamey.hicks@qrclab.com>
2015-01-29 17:47:51 -0500

    Android.mk

---
+++ jni/Android.mk
@@ -0,0 +1,13 @@
+# example Android Native Library makefile
+# contributed by Gregory Junker <ggjunker@gmail.com>
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := webserver
+LOCAL_CFLAGS := -I/scratch/jamey/libwebsockets/lib
+APP_SRC_FILES := ../src/zedboard-server.c
+LOCAL_SRC_FILES := $(APP_SRC_FILES)
+LOCAL_LDLIBS := -L/scratch/jamey/libwebsockets/obj/local/armeabi -lwebsockets
+
+include $(BUILD_EXECUTABLE)
