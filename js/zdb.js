bff12f1e068012e79e0b866f2da4a0a0300ceb8f
Jamey Hicks <jamey.hicks@qrclab.com>
2015-01-30 21:51:42 +0000

    added discover and exit commands

--- js/zdb.js
+++ js/zdb.js
@@ -7,7 +7,9 @@ var Josh = Josh || {};
       }
     };
 
-      var wsUri = 'ws' + root.location.origin.slice(4);
+      var $consolePanel;
+      var $discoveryPanel;
+      var wsUri = 'ws' + root.location.origin.slice(4) + '/ws/';
       _console.log(wsUri);
 
       var itemTemplate = _.template("<% _.each(items, function(item, i) { %><div><%- item %></div><% }); %>");
@@ -58,6 +60,57 @@ var Josh = Josh || {};
 	  }
       };
 
+      function probeAddr(ipaddr, discoveryPanel, shellCallback) {
+	  var uri = 'ws://' + ipaddr + '/ws/';
+	  var websocket = new WebSocket(uri, 'shell');
+	  websocket.onopen = function(evt) {
+	      discoveryPanel.append('<div>Device: ' + ipaddr + '</div>');
+	      wsUri = uri;
+	      shellCallback(_.template('<div>Device uri <%- uri %></div>')({uri: uri}));
+	      websocket.close();
+	  }
+	  websocket.onclose = function(evt) {
+	      websocket = 0;
+	  }
+	  websocket.onerror = function(evt) {
+	      websocket = 0;
+	  }
+      };
+
+    function runDiscovery(shellCallback) {
+	var i;
+	var netaddrs = ['192.168.168.100', '192.168.1.100', '54.86.72.180', '172.17.1.205'];
+	for (n in netaddrs) {
+	    var netaddr = netaddrs[n].split('.');
+	    var firstaddr = parseInt(netaddr[3]);
+	    _console.log(netaddr);
+	    _console.log('firstaddr = ' + firstaddr);
+	    for (var i = 0; i < 10; i++) {
+		_console.log(i + firstaddr);
+		netaddr[3] = i + firstaddr;
+		console.log(netaddr);
+		probeAddr(netaddr.join('.'), $discoveryPanel, shellCallback);
+	    }
+	}
+    };
+
+
+      var activateConsolePanel = function() {
+	  _console.log("activating shell");
+	  shell.activate();
+	  $consolePanel.slideDown();
+	  $consolePanel.focus();
+      };
+
+      // Whenever we get either a `EOT` (`Ctrl-D` on empty line) or a `Cancel` (`Ctrl-C`) signal from the shell,
+      // we deactivate the shell and hide the console.
+      function hideAndDeactivate() {
+          _console.log("deactivating shell");
+          shell.deactivate();
+          $consolePanel.slideUp();
+          $consolePanel.blur();
+      }
+
     // Create `History` and `KillRing` by hand since we will use the `KillRing` for an example command.
     var history = Josh.History();
     var killring = new Josh.KillRing();
@@ -85,6 +138,17 @@ var Josh = Josh || {};
 		  pullFile(args[0], callback);
 	  }
       });
+      shell.setCommandHandler("discover", {
+	  exec: function(cmd, args, callback) {
+	      _console.log("starting device discovery");
+	      runDiscovery(callback);
+	  }
+      });
+      shell.setCommandHandler("exit", {
+	  exec: function(cmd, args, callback) {
+	      hideAndDeactivate();
+	  }
+      });
 
     // Setup Document Behavior
     // -----------------------
@@ -96,14 +160,9 @@ var Josh = Josh || {};
 	// the shell config parameter `shell-panel-id`. The `Shell` display model relies on a 'panel' to contain a 'view'.
 	// The 'panel' acts as the view-port, i.e. the visible portion of the shell content, while the 'view' is appended
 	// to and scrolled up as new content is added.
-	var $consolePanel = $('#shell-panel');
-
-	var activateConsolePanel = function() {
-	    _console.log("activating shell");
-	    shell.activate();
-	    $consolePanel.slideDown();
-	    $consolePanel.focus();
-	};
+	$consolePanel = $('#shell-panel');
+	$discoveryPanel = $('#discovery-panel');
+	$discoveryPanel.append('<div>discovery panel</div');
 
 	// We use **jquery-ui**'s `resizable` to let us drag the bottom edge of the console up and down.
 	$consolePanel.resizable({ handles: "s"});
@@ -122,15 +181,6 @@ var Josh = Josh || {};
 		activateConsolePanel();
             }
 	});
-
-	// Whenever we get either a `EOT` (`Ctrl-D` on empty line) or a `Cancel` (`Ctrl-C`) signal from the shell,
-	// we deactivate the shell and hide the console.
-	function hideAndDeactivate() {
-            _console.log("deactivating shell");
-            shell.deactivate();
-            $consolePanel.slideUp();
-            $consolePanel.blur();
-	}
 
 	// Attach our hide function to the EOT and Cancel events.
 	shell.onEOT(hideAndDeactivate);
