dd39e5b897dfb0d4b52742d03b93a8dbc4945dd2
Jamey Hicks <jamey.hicks@qrclab.com>
2015-02-13 12:42:12 -0500

    added -llog to LOCAL_LDLIBS

--- jni/Android.mk
+++ jni/Android.mk
@@ -8,6 +8,6 @@ LOCAL_MODULE := webserver
 LOCAL_CFLAGS := -I/scratch/jamey/libwebsockets/lib
 APP_SRC_FILES := ../src/zedboard-server.c
 LOCAL_SRC_FILES := $(APP_SRC_FILES)
-LOCAL_LDLIBS := -L/scratch/jamey/libwebsockets/obj/local/armeabi -lwebsockets
+LOCAL_LDLIBS := -L/scratch/jamey/libwebsockets/obj/local/armeabi -lwebsockets -llog
 
 include $(BUILD_EXECUTABLE)
